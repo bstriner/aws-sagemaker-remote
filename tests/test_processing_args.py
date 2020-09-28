f87b1073bf62abc0be6f0fdd729cb0f8c12e73f3
Benjamin Striner <bstriner@gmail.com>
2020-09-27 20:20:03 -0400

    fixtests

--- tests/test_processing_args.py
+++ tests/test_processing_args.py
@@ -3,14 +3,16 @@ from aws_sagemaker_remote.processing.args import sagemaker_processing_input_args
 
 from aws_sagemaker_remote.processing.args import sagemaker_processing_output_args
 
+from aws_sagemaker_remote.args import convert_path_arguments
+
 def test_processing_inputs():
     parser = argparse.ArgumentParser()
     sagemaker_processing_input_args(
         parser=parser,
-        inputs={
+        inputs=convert_path_arguments({
             'dataset1':'data/ds1',
             'dataset2':'data/ds2'
-        }
+        })
     )
     args=parser.parse_args(args=['--dataset2','data/ds3'])
     assert args.dataset1 == 'data/ds1'
@@ -20,10 +22,10 @@ def test_processing_outputs():
     parser = argparse.ArgumentParser()
     sagemaker_processing_output_args(
         parser=parser,
-        outputs={
-            'dataset1':('data/ds1','default'),
-            'dataset2':('data/ds2','default')
-        }
+        outputs=convert_path_arguments({
+            'dataset1':'data/ds1',
+            'dataset2':'data/ds2'
+        })
     )
     args=parser.parse_args(args=['--dataset2','data/ds3'])
     assert args.dataset1 == 'data/ds1'
